f17c17f7556e6f5fa1178da510cbed60a221dfeb
yoshimitsuy <rafael.lacana7@gmail.com>
2023-03-03 19:22:56 -0300

    card pokemon and pokemon list construction

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { PokemonContext } from '../context/PokemonContext';
+import { CardPokemon } from './CardPokemon';
 
 export const PokemonList = () => {
+
+  const { allPokemons } = useContext(PokemonContext) // aqui deu merda
   return (
-    <div>PokemonList</div>
+    <>
+
+      <div className="card-list-pokemon container">
+
+        { allPokemons.map(pokemon => (
+          <CardPokemon pokemon={ pokemon } key={ pokemon.id }/>
+        )) };
+
+      </div>
+
+    </>
   )
 }
