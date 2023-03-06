8f13c0d6d35ec30dd9467f6cab728be15adee3e7
yoshimitsuy <rafael.lacana7@gmail.com>
2023-03-06 01:37:27 -0300

    css loader, loader comp, loader in pokemon list in progress

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,21 +1,32 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { PokemonContext } from '../context/PokemonContext';
 import { CardPokemon } from './CardPokemon';
+import { Loader } from './Loader';
 
 export const PokemonList = () => {
 
-  const { allPokemons } = useContext(PokemonContext) // aqui deu merda
+  const { allPokemons, loading } = useContext(PokemonContext);
+  
+  // const [loading, setLoading] = useState(true);
+  
   return (
     <>
 
-      <div className="card-list-pokemon container">
+      {
+        loading ? (
+          <Loader />
+        ) : (
+              <div className="card-list-pokemon container">
 
-        { allPokemons.map(pokemon => (
-          <CardPokemon pokemon={ pokemon } key={ pokemon.id }/>
-        )) };
+                    { 
+                      allPokemons.map(pokemon => (
+                      <CardPokemon pokemon={ pokemon } key={ pokemon.id }/>
+                      )) 
+                    }
 
-      </div>
-
+              </div>
+        )
+      }
     </>
-  )
-}
+  );
+};
